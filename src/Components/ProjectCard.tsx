c219d827c4727ae3f08c8910f0bc3dfb2f6aa409
sloorjuice <antant8085@gmail.com>
2025-04-21 21:45:09 -0400

    Fixed github logo glitch

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -27,14 +27,13 @@ export default function ProjectCard({ title, description, image, projectLink, gi
       />
       <div className="p-4">
         <h3 className="text-xl font-bold text-white mb-2">{title}</h3>
-        <p className="text-gray-400">{description}</p>
+        <p className="text-gray-400 pb-4">{description}</p> {/* Add padding-bottom */}
         <a
           href={projectLink}
           target="_blank"
           rel="noopener noreferrer"
           className="text-blue-500 hover:text-gray-300"
         >
-          View Project
         </a>
         {githubLink && (
           <a
@@ -42,7 +41,7 @@ export default function ProjectCard({ title, description, image, projectLink, gi
             target="_blank"
             rel="noopener noreferrer"
             onClick={(e) => e.stopPropagation()} // Prevent parent click
-            className="absolute bottom-4 right-4 text-blue-500 hover:text-gray-300"
+            className="absolute bottom-1.5 right-4 text-blue-500 hover:text-gray-300"
           >
             <FaGithub size={24} />
           </a>
