fabb566aaa424829e6552e24fc2b62f3dd0a242b
sloorjuice <antant8085@gmail.com>
2025-04-28 20:32:53 -0400

    Added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: "https://sloor.dev",
+    generateRobotsTxt: true, // optional
+  };
+  
+  module.exports = config;
+  
